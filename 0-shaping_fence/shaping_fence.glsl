5573426e7e943875f60b8caf8f97ed165e99134e
hyperdestru <slotuser@gmail.com>
2022-05-28 16:10:31 +0200

    Reformat.

--- 0-shaping_fence/shaping_fence.glsl
+++ 0-shaping_fence/shaping_fence.glsl
@@ -1,8 +1,3 @@
-/*******************************************************************/
-/* This is part of my shader training with the wonderful book      */
-/* https://thebookofshaders.com                                    */
-/*******************************************************************/
-
 #ifdef GL_ES
 precision mediump float;
 #endif
@@ -15,99 +10,92 @@ uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 uniform float u_time;
 
-/*******************************************************************/
-/* Shaping functions from Inigo Quilez                             */
-/* https://www.iquilezles.org/                                     */
-/*******************************************************************/
+// #######################################
+// # Shaping functions from Inigo Quilez #
+// #    https://www.iquilezles.org       #
+// #######################################
 
-float impulse(float k, float x) {
-    float h = k * x;
-    return h * exp(1.0 - h);
+float impulse(float k,float x){
+    float h=k*x;
+    return h*exp(1.-h);
 }
 
-float expImpulse(float x, float k) {
-    float h = k * x;
-    return h * exp(1.0 - h);
+float expImpulse(float x,float k){
+    float h=k*x;
+    return h*exp(1.-h);
 }
 
-float parabola(float x, float k){
-    return pow(4.0 * x * (1.0 - x), k);
+float parabola(float x,float k){
+    return pow(4.*x*(1.-x),k);
 }
 
-float cubicPulse(float c, float w, float x) {
-    x = abs(x - c);
-    if (x > w) return 0.0;
-    x /= w;
-    return 1.0 - x * x * (3.0 - 2.0 * x);
+float cubicPulse(float c,float w,float x){
+    x=abs(x-c);
+    if(x>w)return 0.;
+    x/=w;
+    return 1.-x*x*(3.-2.*x);
 }
 
-float almostIdentity(float x, float m, float n) {
-    if (x > m) return x;
-    float a = 2.0 * n - m;
-    float b = 2.0 * m - 3.0 * n;
-    float t = x / m;
-    return (a * t + b) * t * t + n;
+float almostIdentity(float x,float m,float n){
+    if(x>m)return x;
+    float a=2.*n-m;
+    float b=2.*m-3.*n;
+    float t=x/m;
+    return(a*t+b)*t*t+n;
 }
 
-float almostUnitIdentity(float x) {
-    return x * x * (2.0 - x);
+float almostUnitIdentity(float x){
+    return x*x*(2.-x);
 }
 
-/*******************************************************************/
+// #########################################
+// # Shaping functions with Kynd equations #
+// #########################################
 
-
-/*******************************************************************/
-/* Shaping functions with Kynd equations                           */
-/*******************************************************************/
-
-float kynd1(float x, float expo) {
-    return 1.0 - pow(abs(x), expo);
+float kynd1(float x,float expo){
+    return 1.-pow(abs(x),expo);
 }
 
-float kynd2(float x, float expo) {
-    return pow(cos(PI * x / 2.0), expo);
+float kynd2(float x,float expo){
+    return pow(cos(PI*x/2.),expo);
 }
 
-float kynd3(float x, float expo) {
-    return 1.0 - pow(abs(sin(PI * x / 2.0)), expo);
+float kynd3(float x,float expo){
+    return 1.-pow(abs(sin(PI*x/2.)),expo);
 }
 
-float kynd4(float x, float expo) {
-    return pow(min(cos(PI * x / 2.0), 1.0 - abs(x)), expo);
+float kynd4(float x,float expo){
+    return pow(min(cos(PI*x/2.),1.-abs(x)),expo);
 }
 
-float kynd5(float x, float expo) {
-    return 1.0 - pow(max(0.0, abs(x) * 2.0 - 1.0), expo);
+float kynd5(float x,float expo){
+    return 1.-pow(max(0.,abs(x)*2.-1.),expo);
 }
 
-/*******************************************************************/
+// #######################
+// # Utilitary functions #
+// #######################
 
-
-/*******************************************************************/
-/* Utilitary functions                                             */
-/*******************************************************************/
-
-/**
- * Trace a line.
- * Returns the difference between 2 smoothstep calls.
- * The smoothstep function perform smooth Hermite interpolation between two values:
- * 1st argument specifies the value of the lower edge of the Hermite function
- * 2nd argument specifies the value of the upper edge of the Hermite function
- * 3nd argument specifies the source value for interpolation (here it's st.y)  
- */
-float plot(vec2 st, float y) {
-    float w = 0.009;
-    return smoothstep(y - w, y, st.y) - smoothstep(y, y + w, st.y);
+// Trace a line.
+// Returns the difference between 2 smoothstep calls.
+float plot(vec2 st,float y){
+    float w=.009;
+    // The smoothstep function perform smooth Hermite
+    // interpolation between two values:
+    // 1st arg specifies the value of the lower edge of the
+    // Hermite function.
+    // 2nd arg specifies the value of the upper edge of the
+    // Hermite function.
+    // 3nd arg specifies the source value for
+    // interpolation (here it's st.y).
+    return smoothstep(y-w,y,st.y)-smoothstep(y,y+w,st.y);
 }
 
-/*******************************************************************/
-
-
-void main() {
-    vec2 st = gl_FragCoord.xy / u_resolution;
+void main(){
+    vec2 st=gl_FragCoord.xy/u_resolution;
     
     float y;
-    y = sin(st.x);
+    //y = sin(st.x);
     //y = cos(st.x);
     //y = sin(u_time + st.x);
     //y = sin(u_time * st.x);
@@ -119,7 +107,7 @@ void main() {
     //y = fract(sin(st.x));
     //y = ceil(sin(st.x));
     //y = floor(sin(st.x));
-
+    
     //y = mod(st.x, 0.5); // return x modulo of 0.5
     //y = fract(st.x); // return only the fraction part of a number
     //y = ceil(st.x); // nearest integer that is greater than or equal to x
@@ -129,15 +117,15 @@ void main() {
     //y = clamp(st.x, 0.0, 1.0); // constrain x to lie between 0.0 and 1.0
     //y = min(0.0, st.x); // return the lesser of x and 0.0
     //y = max(0.0, st.x); // return the greater of x and 0.0
-
+    
     // Shaping with Inigo functions
     //y = impulse(12.0, st.x);
     //y = almostIdentity(st.x, 0.4, 0.);
     //y = almostUnitIdentity(st.x);
     //y = expImpulse(st.x, 10.0);
     //y = cubicPulse(0.5, 0.2, st.x);
-    //y = parabola(st.x, 1.0);
-
+    y=parabola(st.x,1.);
+    
     // Shaping with kynd functions
     //y = kynd1(st.x, 0.5);
     //y = kynd2(st.x, 3.5);
@@ -145,13 +133,13 @@ void main() {
     //y = kynd4(st.x, 0.5);
     //y = kynd5(st.x, 1.0);
     
-    float pct = plot(st, y);
-
-    vec3 bg = vec3(y);
-
-    vec3 linecolor = vec3(1.0, 0.0, 0.0);
-
-    vec3 finalcolor = ((1.0 - pct) * bg) + (pct * linecolor);
+    float pct=plot(st,y);
     
-    gl_FragColor = vec4(finalcolor, 1.0);
+    vec3 bg=vec3(y);
+    
+    vec3 linecolor=vec3(1.,0.,0.);
+    
+    vec3 finalcolor=((1.-pct)*bg)+(pct*linecolor);
+    
+    gl_FragColor=vec4(finalcolor,1.);
 }
--- 1-color_feeling/color_feeling.glsl
+++ 1-color_feeling/color_feeling.glsl
@@ -1,30 +1,42 @@
-// Make an expressive transition between colors. 
-// Think of a particular emotion. What color seems most representative of it? 
-// How does it appear? How does it fade away? 
-// Think of another emotion and the matching color for it. 
-// Change the beginning and ending color of the above code to match those emotions. 
-// Then animate the transition using shaping functions. 
-// Robert Penner developed a series of popular shaping functions for computer animation 
-// known as easing functions, you can use this example as research and inspiration 
+// Make an expressive transition between colors.
+// Think of a particular emotion. What color seems most representative of it?
+// How does it appear? How does it fade away?
+// Think of another emotion and the matching color for it.
+// Change the beginning and ending color of the above code to match those emotions.
+// Then animate the transition using shaping functions.
+// Robert Penner developed a series of popular shaping functions for computer animation
+// known as easing functions, you can use this example as research and inspiration
 // but the best result will come from making your own transitions.
 
 #ifdef GL_ES
 precision mediump float;
 #endif
 
+#define PI 3.14159265359
+#define PIH 1.57079632679
+#define PI180 565.486677646
+
 uniform vec2 u_resolution;
 uniform float u_time;
 
-vec3 colorA=vec3(.149,.141,.912);
-vec3 colorB=vec3(1.,.833,.224);
+float impulse(float k,float x){
+    float h=k*x;
+    return h*exp(1.-h);
+}
+
+float kynd2(float x,float expo){
+    return pow(cos(PI*x/2.),expo);
+}
+
+vec3 colorA=vec3(.3765,0.,.9882);
+vec3 colorB=vec3(.8745,1.,.3059);
 
 void main(){
     vec3 color=vec3(0.);
     
     float pct=abs(sin(u_time));
     
-    // Mix uses pct (a value from 0-1) to
-    // mix the two colors
+    // Mix uses pct (a value from 0-1) to mix the two colors
     color=mix(colorA,colorB,pct);
     
     gl_FragColor=vec4(color,1.);

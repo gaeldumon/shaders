d86c9ba0fc26f68fc6a8c1d688b1182e0a1f3da3
gaeldumon <dmnleag@protonmail.com>
2023-12-22 11:04:23 +0100

    Reorg. files and rework beach

--- src/beach.glsl
+++ src/beach.glsl
@@ -4,19 +4,19 @@ precision mediump float;
 
 uniform vec2 u_resolution;
 
-vec3 colorA = vec3(0.8471, 0.7765, 0.5608);
-vec3 colorB = vec3(0.1216, 0.3725, 0.9137);
+vec3 sand = vec3(0.8471, 0.7765, 0.5608);
+vec3 water = vec3(0.1216, 0.3725, 0.9137);
 
 void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
     
     vec3 pct = vec3(st.y);
     
-    pct.r = st.y * 4.0;
-    pct.g = st.y;
-    pct.b = st.y * 3.8;
+    pct.r = st.x * st.y * 4.0;
+    pct.g = st.x * st.y;
+    pct.b = st.x * st.y * 6.0;
     
-    vec3 color = mix(colorA, colorB, pct);
+    vec3 color = mix(sand, water, pct);
     
     gl_FragColor = vec4(color, 1.0);
 }

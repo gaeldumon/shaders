5143b4e3145d7e4a3111776865c8781ac980e6d2
gaeldumon <dmnleag@protonmail.com>
2024-01-19 14:51:40 +0100

    Cleaned checker

--- src/checkerboard.glsl
+++ src/checkerboard.glsl
@@ -15,18 +15,17 @@ void main() {
     // Normalized fragment coordinates
     vec2 uv = (gl_FragCoord.xy * 2.0 - u_resolution) / u_resolution.y;
     
-    vec2 effectCenter = vec2(sin((u_time * 0.5)), cos((u_time * 0.5)));
+    // Goes around the surface following borders
+    vec2 effectCenter = vec2(sin((u_time * 0.35)), cos((u_time * 0.35)));
     
-    // sin(time) oscillates between -1 and 1. By adding 1 to it, we make it oscillate between 0 and 2.
-    // Multiplying by 0.1 scales the oscillation to be between 0 and 0.2.
-    // Adding 0.2 shifts the oscillation to be between 0.2 and 0.4.
-    float effectRadius = 0.2 + 2.0 * (1.0 + sin(u_time * 0.5));
+    float effectRadius = 0.4;
     
-    float effectStrength = 1.5;
+    float effectStrength = 0.2;
     
     float distance = length(uv - effectCenter);
     
     float distortion = 1.0 + (effectStrength * (effectRadius - distance));
+    
     vec2 distortedUV = effectCenter + (uv - effectCenter) * distortion;
     
     gl_FragColor = vec4(vec3(checkerboard(distortedUV)), 1.0);

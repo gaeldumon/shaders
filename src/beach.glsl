9e3fcd1f2e24f600f592458dd8d995b4d86b9670
gaeldumon <dmnleag@protonmail.com>
2024-01-28 14:30:29 +0100

    Include view code links and beach update

--- src/beach.glsl
+++ src/beach.glsl
@@ -3,18 +3,27 @@ precision mediump float;
 #endif
 
 uniform vec2 u_resolution;
+uniform float u_time;
 
 vec3 sand = vec3(0.8471, 0.7765, 0.5608);
 vec3 water = vec3(0.1216, 0.3725, 0.9137);
+
+float waving() {
+    float wave = sin(u_time * 0.6) * 0.35 + 0.5;
+    return wave;
+}
 
 void main() {
     vec2 st = gl_FragCoord.xy / u_resolution.xy;
     
     vec3 pct = vec3(st.y);
     
-    pct.r = st.x * st.y * 4.0;
-    pct.g = st.x * st.y;
-    pct.b = st.x * st.y * 6.0;
+    // Red channel
+    pct.r = st.x * st.y * 4.0 * waving();
+    // Green channel
+    pct.g = st.x * st.y * waving();
+    // Blue channel
+    pct.b = st.x * st.y * 6.0 * waving();
     
     vec3 color = mix(sand, water, pct);
     
